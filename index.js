66a63aa4b0a2a60a5cfff29cde88d261c2065b94
user <user@ROBOT.local>
2022-06-05 09:46:47 +0700

    done

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, callbackSort) => 
+  callbackSort(names).map((name, i) => `${i + 1}. ${name}`);
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort((a, b) => a.localeCompare(b));
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  return names.sort((a, b) => b.localeCompare(a));
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
